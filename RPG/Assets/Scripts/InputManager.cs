8b29974de85929b4bbdfaa47a622e3cde2556ab3
LironeA <322004@ukr.net>
2022-01-26 13:26:37 +0200

    Writed scripts

---
+++ RPG/Assets/Scripts/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyBox;
+
+public class InputManager : MonoBehaviour
+{	
+	[HideInInspector][AutoProperty]
+	public Character character;
+	[SearchableEnum]
+	public KeyCode selectTile;
+	[SearchableEnum]
+	public KeyCode moveToTile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if(Input.GetKeyDown(selectTile))
+	    {
+	    	character.SelectTile();
+	    }
+	    if(Input.GetKeyDown(moveToTile))
+	    {
+	    	character.MoveToTile();
+	    }
+    }
+}
