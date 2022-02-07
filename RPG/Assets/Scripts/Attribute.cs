efb2109230d30cc1b830943d86718ef81050d41d
LironeA <322004@ukr.net>
2022-02-07 20:58:32 +0200

    Writed Attributet system
    Writed: Attribute.cs, Attributes.cs

---
+++ RPG/Assets/Scripts/Attribute.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Attributes", menuName = "ScriptableObjects/Attributes", order = 1)]
+public class Attribute : ScriptableObject
+{
+}
--- RPG/Assets/Scripts/Character.cs
+++ RPG/Assets/Scripts/Character.cs
@@ -1,23 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using MyBox;
 
 public class Character : MonoBehaviour
 {
     [HideInInspector] [AutoProperty] public MovementController movementController;
     [HideInInspector] [AutoProperty] public SelectionController selectionController;
-
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-    }
 
     public void SelectTile()
     {
